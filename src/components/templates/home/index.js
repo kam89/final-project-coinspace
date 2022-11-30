368c5f6e328f7aa591ee58d51ebbbddeabc8be45
K M <30794641+kam89@users.noreply.github.com>
2022-12-01 00:37:21 +0800

    :construction: Add new Home template

---
+++ src/components/templates/home/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { Container } from '@mui/material';
+import { api } from 'api';
+
+
+
+export const Home = () => {
+
+  // useEffect(() => {
+  //   getCoinsWithGlobalAveragePrice();
+  // }, []);
+
+  // const getCoinsWithGlobalAveragePrice = async () => {
+  //   const result = await api.get(`coins?skip=0&limit=5&currency=EUR`);
+  //   if (result.status === 200) 
+  // };
+
+  return (
+    <Container>
+
+    </Container>
+  );
+};
