47c762a4e059981b7180b8440444dc4cd282c456
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-11-30 16:32:19 +0800

    💄 Add defaultThemes in themes

---
+++ src/themes/defaultThemes.js
@@ -0,0 +1,22 @@
+export const defaultThemes = {
+  palette: {
+    primary: {
+      light: '#bdf478',
+      main: '#8bc34a',
+      dark: '#599014',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#ffffff',
+    },
+  },
+  typography: {
+    fontSize: 16,
+    button: {
+      textTransform: 'none'
+    }
+  },
+};
