71a7ea1ed601dd07f2d7dbae2bc3d4af6cde2a18
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-11-30 16:33:07 +0800

    💄 Update App to use ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import './App.css';
+import React from 'react';
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  responsiveFontSizes,
+  ThemeProvider
+} from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { defaultThemes } from 'themes';
 
 function App() {
+  let theme = createTheme(defaultThemes);
+  theme = responsiveFontSizes(theme);
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Container maxWidth="lg">
+      </Container>
+    </ThemeProvider>
   );
 }
 
