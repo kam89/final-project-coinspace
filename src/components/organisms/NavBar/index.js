4fb046d212fd51ff8c2e32f4d7c941307d2a21df
K M <30794641+kam89@users.noreply.github.com>
2022-12-06 23:17:30 +0800

    :zap: Update NavBar to add description

--- src/components/organisms/NavBar/index.js
+++ src/components/organisms/NavBar/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  AppBar,
-  Toolbar,
-  Typography
-} from '@mui/material';
-import { Player } from "@lottiefiles/react-lottie-player";
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { Player } from '@lottiefiles/react-lottie-player';
 
-export const NavBar = ({ appName }) => {
+export const NavBar = ({ appName, description }) => {
   return (
-    <AppBar position='static'>
+    <AppBar position="static">
       <Toolbar disableGutters>
         <Player
           autoplay
@@ -17,25 +13,27 @@ export const NavBar = ({ appName }) => {
           src="https://assets7.lottiefiles.com/packages/lf20_3bhqygev.json"
           style={{ height: 100, width: 100 }}
         />
-        <Typography
-          variant="h6"
-          noWrap
-          sx={{
-            display: 'flex',
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          {appName}
-        </Typography>
+        <Box>
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              display: 'flex',
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}>
+            {appName}
+          </Typography>
+          <Typography>{description}</Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
 };
 
 NavBar.propTypes = {
-  appName: PropTypes.string.isRequired
+  appName: PropTypes.string.isRequired,
 };
