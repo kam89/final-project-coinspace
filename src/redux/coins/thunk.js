c9ba361386bf49c62038fff6a350459f685a1e17
K M <30794641+kam89@users.noreply.github.com>
2022-12-07 18:40:38 +0800

    :zap: Update getCoinsWithGlobalAveragePrice thunk in coins

--- src/redux/coins/thunk.js
+++ src/redux/coins/thunk.js
@@ -1,7 +1,12 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { api } from "api";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { api } from 'api';
 
-export const getCoinsWithGlobalAveragePrice = createAsyncThunk('getCoinsWithGlobalAveragePrice', async (coins, { rejectWithValue }) => {
-  const response = await api.get(`coins?skip=0&limit=5&currency=EUR`);
-  if (response.status === 200) return response.data;
-});
+export const getCoinsWithGlobalAveragePrice = createAsyncThunk(
+  'getCoinsWithGlobalAveragePrice',
+  async (coins, { rejectWithValue }) => {
+    const response = await api.get(
+      `coins?skip=0&limit=5&currency=${coins.currency}`
+    );
+    if (response.status === 200) return response.data;
+  }
+);
