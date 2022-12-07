89762614547fc3d07a46f51d23bb3a7c063f4f20
K M <30794641+kam89@users.noreply.github.com>
2022-12-07 19:52:19 +0800

    :card_file_box: Update reducer in coins

--- src/redux/coins/selector.js
+++ src/redux/coins/selector.js
@@ -1,6 +1,11 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getCoins = createSelector(
   (state) => state.coins.coins,
-  coins => coins
+  (coins) => coins
+);
+
+export const getCoinsStatus = createSelector(
+  (state) => state.coins.status,
+  (status) => status
 );
