d5eeb7292b64a8d4721fa77329423721b4863abe
K M <30794641+kam89@users.noreply.github.com>
2022-12-10 03:14:20 +0800

    :card_file_box: Add thunk, reducer and selector for allCurrencies

--- src/redux/settings/reducer.js
+++ src/redux/settings/reducer.js
@@ -1,8 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { STATUS } from 'api';
 import { currencies } from 'components/templates/home/data';
+import { getFiats } from './thunk';
 
 const initialState = {
   currency: currencies.USD,
+  allCurrencies: [],
 };
 
 const slice = createSlice({
@@ -13,6 +16,18 @@ const slice = createSlice({
     updateCurrency: (state, { payload }) => {
       state.currency = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getFiats.fulfilled, (state, { payload }) => {
+      state.status = STATUS.SUCCESS;
+      state.allCurrencies = payload;
+    });
+    builder.addCase(getFiats.pending, (state, { payload }) => {
+      state.status = STATUS.LOADING;
+    });
+    builder.addCase(getFiats.rejected, (state, { payload }) => {
+      state.status = STATUS.FAIL;
+    });
   },
 });
 
--- src/redux/settings/selector.js
+++ src/redux/settings/selector.js
@@ -4,3 +4,14 @@ export const getCurrency = createSelector(
   (state) => state.settings.currency,
   (currency) => currency
 );
+
+export const getAllCurrencies = createSelector(
+  (state) => state.settings.allCurrencies,
+  (allCurrencies) => allCurrencies
+);
+
+export const getCurrencyByName = (name) =>
+  createSelector(
+    (state) => state.settings.allCurrencies,
+    (allCurrencies) => allCurrencies.find((item) => item.name === name)
+  );
