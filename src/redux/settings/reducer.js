f2bddd155300344de354770545bfd695eb815286
K M <30794641+kam89@users.noreply.github.com>
2022-12-09 18:39:37 +0800

    :card_file_box: Add reducer for settings

---
+++ src/redux/settings/reducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { currencies } from 'components/templates/home/data';
+
+const initialState = {
+  currency: currencies.EUR,
+};
+
+const slice = createSlice({
+  name: 'settings',
+  initialState,
+  reducers: {
+    settingsReset: (_) => initialState,
+    updateSettingsObject: (state, { payload }) => {
+      for (const key in payload) {
+        state[key] = payload[key];
+      }
+    },
+  },
+});
+
+export const { settingsReset } = slice.actions;
+export const settingsReducer = slice.reducer;
