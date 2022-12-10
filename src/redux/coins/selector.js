7f7f9c9c827e8d9aba148e0de46bcde969414f82
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-12-10 21:51:26 +0800

    🗃️ Add new selector for coins

--- src/redux/coins/selector.js
+++ src/redux/coins/selector.js
@@ -9,3 +9,13 @@ export const getCoinsStatus = createSelector(
   (state) => state.coins.status,
   (status) => status
 );
+
+export const getHistoryStatus = createSelector(
+  (state) => state.coins.historyStatus,
+  (status) => status
+);
+
+export const getSelectedCoin = createSelector(
+  (state) => state.coins.selectedCoin,
+  (selectedCoin) => selectedCoin
+);
