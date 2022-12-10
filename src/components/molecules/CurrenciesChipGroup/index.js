49ed5544f4781e3967ef6f3a84c5ef2be299f48d
K M <30794641+kam89@users.noreply.github.com>
2022-12-10 13:47:26 +0800

    :zap: Update CurrenciesChipGroup molecule

--- src/components/molecules/CurrenciesChipGroup/index.js
+++ src/components/molecules/CurrenciesChipGroup/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Stack, Chip } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Stack, Chip, Avatar } from '@mui/material';
+import { useSelector } from 'react-redux';
 import { famousCurrencies } from 'components/templates/home/data';
+import { getAllCurrencies } from 'redux/settings/selector';
 
 export const CurrenciesChipGroup = ({ currency, onClick }) => {
+  const allCurrencies = useSelector(getAllCurrencies);
+  const [currencyStack, setCurrencyStack] = useState([]);
+
+  useEffect(() => {
+    if (allCurrencies.length === 0) return;
+    getCurrencyDetail();
+  }, [allCurrencies]);
+
+  const getCurrencyDetail = () => {
+    const newStack = allCurrencies.filter(
+      (item) => item.name === famousCurrencies[item.name]
+    );
+    setCurrencyStack(newStack);
+  };
+
   return (
     <Stack alignItems={'flex-end'}>
       <Stack direction="row" spacing={1}>
-        {Object.values(famousCurrencies).map((item, index) => (
+        {currencyStack?.map((item, index) => (
           <Chip
             variant="filled"
-            color={currency === item ? 'primary' : 'secondary'}
+            color={item?.name === currency ? 'primary' : 'secondary'}
             key={index}
-            label={item}
-            onClick={() => onClick(item)}
+            label={item?.name}
+            avatar={<Avatar alt={item?.name} src={item?.imageUrl} />}
+            onClick={() => onClick(item?.name)}
           />
         ))}
       </Stack>
