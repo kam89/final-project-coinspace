1d087c15bb61b72dac1b2507377acabd719d89ea
K M <30794641+kam89@users.noreply.github.com>
2022-12-10 13:58:17 +0800

    :zap: Update CustomTooltipRecharts molecule

--- src/components/molecules/CustomTooltipRecharts/index.js
+++ src/components/molecules/CustomTooltipRecharts/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { getCurrency } from 'redux/settings/selector';
+import { getCurrency, getCurrencyByName } from 'redux/settings/selector';
 import { formatAmount } from 'function';
 
 export const CustomTooltipRecharts = ({ active, payload, label }) => {
   const theme = useTheme();
   const currency = useSelector(getCurrency);
+  const currencyDetail = useSelector(getCurrencyByName(currency));
 
   if (active && payload && payload.length) {
     const date = new Date(label * 1000);
@@ -22,7 +23,7 @@ export const CustomTooltipRecharts = ({ active, payload, label }) => {
           {newDateFormatted[0] + ' ' + newDateFormatted[1].substring(0, 5)}
         </Typography>
         <Typography variant="caption">{`USD : ${formatAmount(
-          currency,
+          currencyDetail,
           payload[0].payload.price
         )}`}</Typography>
       </Box>
