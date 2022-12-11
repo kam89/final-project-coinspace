92dbcc370a65426f72a4b0e5da54f6cbb2ed8d0a
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-12-11 12:57:59 +0800

    🚨 Fix for eslint warning

--- src/components/templates/CoinDetail/index.js
+++ src/components/templates/CoinDetail/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ArrowBackIosNew } from '@mui/icons-material';
 
@@ -20,7 +10,7 @@ import { HistoricalPriceChart } from 'components/organisms/HistoricalPriceChart'
 import { MarketTable } from 'components/organisms/MarketTable';
 import { News } from 'components/organisms/News';
 
-export const CoinDetail = ({}) => {
+export const CoinDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
