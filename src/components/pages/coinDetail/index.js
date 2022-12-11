48722b66d1bda7644b56c825fd9d6665850b9ebf
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-12-11 12:59:24 +0800

    🚧 Add CoinDetail page

---
+++ src/components/pages/coinDetail/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Button } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ArrowBackIosNew } from '@mui/icons-material';
+
+import { selectedCoin } from './data';
+
+import { CoinDetailCard } from 'components/organisms/CoinDetailCard';
+import { HistoricalPriceChart } from 'components/organisms/HistoricalPriceChart';
+import { MarketTable } from 'components/organisms/MarketTable';
+import { News } from 'components/organisms/News';
+
+export const CoinDetail = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ marginTop: 2 }}>
+      <Button startIcon={<ArrowBackIosNew />} onClick={() => navigate('/')}>
+        Go Back
+      </Button>
+      <CoinDetailCard data={selectedCoin} />
+      <HistoricalPriceChart id={id} />
+      <MarketTable id={id} />
+      <News id={id} />
+    </Box>
+  );
+};
