4c83fc0283f746a16aafa5a60c75f54580eceed4
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-12-11 13:01:05 +0800

    ⚡️ Update coinDetail page

--- src/components/pages/coinDetail/index.js
+++ src/components/pages/coinDetail/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Box, Button } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ArrowBackIosNew } from '@mui/icons-material';
-
-import { selectedCoin } from './data';
+import { useSelector } from 'react-redux';
 
 import { CoinDetailCard } from 'components/organisms/CoinDetailCard';
 import { HistoricalPriceChart } from 'components/organisms/HistoricalPriceChart';
 import { MarketTable } from 'components/organisms/MarketTable';
 import { News } from 'components/organisms/News';
 
+import { getSelectedCoin } from 'redux/coins/selector';
+
 export const CoinDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const selectedCoin = useSelector(getSelectedCoin);
 
   return (
     <Box sx={{ marginTop: 2 }}>
