b7f855e6fadac90a29fca6b6880603a370739468
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-12-11 20:17:09 +0800

    ⚡️ Update News organism

--- src/components/organisms/News/index.js
+++ src/components/organisms/News/index.js
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import {
   Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
+  Button,
   Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Typography,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { getNewsById, getNewsStatus } from 'redux/coins/selector';
+import {
+  getNewsById,
+  getNewsStatus,
+  getOverallNews,
+} from 'redux/coins/selector';
 import { getNewsWithDate } from 'redux/coins/thunk';
+import { Replay } from '@mui/icons-material';
+
+import { STATUS } from 'api';
 
 export const News = ({ id }) => {
   const dispatch = useDispatch();
   const news = useSelector(getNewsById(id));
-  console.log(news);
+  const overallNews = useSelector(getOverallNews);
   const status = useSelector(getNewsStatus);
 
   const date = new Date();
@@ -26,27 +34,101 @@ export const News = ({ id }) => {
     dispatch(getNewsWithDate({ fromDate: today, toDate: oneMonthfromToday }));
   }, []);
 
+  const handleOnRetry = () => {
+    dispatch(getNewsWithDate({ fromDate: today, toDate: oneMonthfromToday }));
+  };
+
+  if (status !== STATUS.SUCCESS)
+    return (
+      <Box>
+        <Typography variant="h5">News</Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: 5,
+            margin: 5,
+          }}>
+          <Button
+            variant="outlined"
+            startIcon={<Replay />}
+            onClick={handleOnRetry}>
+            Retry
+          </Button>
+        </Box>
+      </Box>
+    );
+
   return (
-    <Box>
+    <Box sx={{ marginTop: 5 }}>
       <Typography variant="h5">News</Typography>
-      <Grid container spacing={2} columns={5}>
-        {news?.map((item, index) => (
-          <Grid item xs={1}>
-            <Card key={index} sx={{ maxWidth: 350, height: '100%' }}>
-              <CardActionArea sx={{ height: '100%' }}>
-                <CardMedia
-                  component="img"
-                  src={item.imgURL}
-                  height="200"
-                  width="200"
-                />
-                <CardContent>
-                  <Typography variant="h6">{item.title}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+      <Grid container spacing={2} columns={2}>
+        <Grid item xs={1}>
+          <List sx={{ width: '100%' }}>
+            {news.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemButton>
+                  <ListItemText
+                    primary={item.title}
+                    secondary={
+                      <Box
+                        sx={{
+                          display: 'inline-flex',
+                          justifyContent: 'space-around',
+                        }}>
+                        <Typography
+                          sx={{ marginRight: 2 }}
+                          component="span"
+                          variant="caption"
+                          color="text.secondary">
+                          {new Date(item.feedDate).toLocaleString()}
+                        </Typography>
+                        {item.coins.map((item, index) => (
+                          <Typography
+                            key={index}
+                            sx={{ marginRight: 1 }}
+                            component="span"
+                            variant="body2"
+                            color="secondary.main">
+                            #{item.coinNameKeyWords}
+                          </Typography>
+                        ))}
+                      </Box>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={1}>
+          <List sx={{ width: '100%' }}>
+            {overallNews.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemButton>
+                  <ListItemText
+                    primary={item.title}
+                    secondary={
+                      <Box
+                        sx={{
+                          display: 'inline-flex',
+                          justifyContent: 'space-around',
+                        }}>
+                        <Typography
+                          sx={{ marginRight: 2 }}
+                          component="span"
+                          variant="caption"
+                          color="text.secondary">
+                          {new Date(item.feedDate).toLocaleString()}
+                        </Typography>
+                      </Box>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
       </Grid>
     </Box>
   );
